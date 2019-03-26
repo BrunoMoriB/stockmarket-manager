c61b6d0750924010409b79e8abf66693ee4df311
BrunoMoriB <brunomoribarata@yahoo.com.br>
2019-03-26 00:22:15 -0300

    Alterando parametros PathVariable para RequestParam

--- src/main/java/com/bolsavalores/acao/AcaoResource.java
+++ src/main/java/com/bolsavalores/acao/AcaoResource.java
@@ -3,10 +3,10 @@ package com.bolsavalores.acao;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,8 +21,8 @@ public class AcaoResource {
 		return acaoRepository.findAll();
 	}
 	
-	@GetMapping(value="/{id}")
-	public Acao buscaAcao(@PathVariable(value="id") long id) {
+	@GetMapping()
+	public Acao buscaAcao(@RequestParam(value="id") long id) {
 		return acaoRepository.findById(id);
 	}
 	
--- src/main/java/com/bolsavalores/operacao/OperacaoResource.java
+++ src/main/java/com/bolsavalores/operacao/OperacaoResource.java
@@ -1,13 +1,12 @@
 package com.bolsavalores.operacao;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,8 +21,8 @@ public class OperacaoResource {
 		return operacaoRepository.findAll();
 	} 
 	
-	@GetMapping(value="/{id}")
-	public Operacao buscaOperacao(@PathVariable(value="id") long id) {
+	@GetMapping()
+	public Operacao buscaOperacao(@RequestParam(value="id") long id) {
 		return operacaoRepository.findById(id);
 	} 
 	
