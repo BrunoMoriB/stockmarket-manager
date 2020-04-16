362a8479b9dcba68385d68aed02e20f9432c9eb9
BrunoMoriB <brunomoribarata@yahoo.com.br>
2020-04-16 01:05:22 -0300

    Adicionando regras de negocio ao salvar Balanço

--- src/main/java/com/bolsavalores/resources/BalancoResource.java
+++ src/main/java/com/bolsavalores/resources/BalancoResource.java
@@ -1,6 +1,13 @@
 package com.bolsavalores.resources;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -34,6 +41,10 @@ public class BalancoResource {
 	
 	@PostMapping()
 	public Balanco salvaBalanco(@RequestBody Balanco balanco) {
+		List<Balanco> balancosAnteriores = balancoRepository.findByAcaoId(balanco.getAcao().getId());
+		
+		balanco.setEvolucaoLucroLiquidoMeses(getEvolucaoLucroLiquidoTrimestral(balanco, balancosAnteriores));
+		balanco.setLucroLiquidoAnual(getLucroLiquidoAnual(balanco, balancosAnteriores));
 		return balancoRepository.save(balanco);
 	}
 	
@@ -45,5 +56,75 @@ public class BalancoResource {
 	@GetMapping("/buscaPorAcaoId")
 	public List<Balanco> buscaBalancosByAcaoId(@RequestParam long acaoId){
 		return balancoRepository.findByAcaoId(acaoId);
+	}
+	
+	private long getLucroLiquidoAnual(Balanco balanco, List<Balanco> balancosAnteriores) {
+		Map<Integer, Integer> mapTrimestres = getMapTrimestres(balanco.getData(), new HashMap<Integer, Integer>());
+		return balancosAnteriores.stream()
+					.filter(b -> verificaDatasBalancosAnteriores(b, mapTrimestres))
+					.mapToLong(b -> b.getLucroLiquidoTrimestral())
+					.sum();
+	}
+	
+	private boolean verificaDatasBalancosAnteriores(Balanco balanco, Map<Integer, Integer> mapTrimestres) {
+		Entry<Integer, Integer> resultado = mapTrimestres.entrySet().stream()
+					.filter(mapTri -> mapTri.getKey() == balanco.getData().getYear() && mapTri.getValue() == balanco.getData().getMonthValue())
+					.findAny()
+					.orElse(null);
+		
+		return resultado != null ? true : false;
+	}
+	
+	private Map<Integer, Integer> getMapTrimestres(LocalDate data, Map<Integer, Integer> mapTrimestres){
+		int mes = data.getMonthValue() - 3;
+		int ano = data.getYear();
+		
+		if(mes > 0) {
+			mapTrimestres.put(ano, mes);
+		}else {
+			mes = 12;
+			ano = ano - 1;
+			mapTrimestres.put(ano, mes);
+		}
+		
+		if(mapTrimestres.size() < 3) {
+			LocalDate trimestreAnterior = LocalDate.of(ano, mes, 30);
+			mapTrimestres.putAll(getMapTrimestres(trimestreAnterior, mapTrimestres));
+		}
+		
+		return mapTrimestres;
+	}
+	
+	private double getEvolucaoLucroLiquidoTrimestral(Balanco balanco, List<Balanco> balancosAnteriores) {
+
+		Balanco balancoTrimestralAnterior = getBalancoTrimestralAnoAnterior(balanco, balancosAnteriores);
+		
+		if(balancoTrimestralAnterior == null)
+			return 0;
+		
+		double diferencaLucroLiquidoTri = balanco.getLucroLiquidoTrimestral() - balancoTrimestralAnterior.getLucroLiquidoTrimestral();
+		double evolucaoLucroLiquidoTri	= diferencaLucroLiquidoTri / balancoTrimestralAnterior.getLucroLiquidoTrimestral();
+		
+		return evolucaoLucroLiquidoTri;
+	}
+	
+	private Balanco getBalancoTrimestralAnoAnterior(Balanco balanco, List<Balanco> balancosAnteriores) {
+		LocalDate dataBalanco  = balanco.getData();
+
+		Month mes = dataBalanco.getMonth();
+		int ano   = dataBalanco.getYear();
+		
+		Balanco balancoTrimestralAnoAnterior = balancosAnteriores.stream()
+					.filter(b -> verificaDataBalancoAnterior(b, mes, ano))
+					.findAny()
+					.orElse(null);
+		
+		return balancoTrimestralAnoAnterior;
+	}
+	
+	private boolean verificaDataBalancoAnterior(Balanco balanco, Month mes, int ano) {
+		LocalDate dataBalanco = balanco.getData();
+		int anoAnterior		  = ano - 1;
+		return mes.compareTo(dataBalanco.getMonth()) == 0 && dataBalanco.getYear() == anoAnterior ? true : false;
 	}
 }
