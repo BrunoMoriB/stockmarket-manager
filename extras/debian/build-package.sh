e95b216038367594fe0f259372a1fc07590a841e
Murilo Arantes <murilo.arantess@gmail.com>
2020-08-18 23:11:57 -0300

    Alterações no build-package para execução na própria máquina

--- extras/debian/build-package.sh
+++ extras/debian/build-package.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if [[ "$(pwd)" != "/stockmarket-manager" ]]; then
-    echo "Execute 'cd /stockmarket-manager' and try to build again";
+pwd | grep -Eq '/stockmarket-manager$'
+if [[ "$?" != "0" ]]; then
+    echo "Acesse a raiz do projet e execute o script novamente";
     exit 1
 fi
 
+PROJECT_DIR=$(pwd)
 TEMP_DIR="$(mktemp -d)"
 BASE_DIR=${TEMP_DIR}"/stockmarket-manager"
-TARGET_DEBIAN_DIR="/stockmarket-manager/target"
-CURRENT_POM_VERSION=$(/usr/bin/mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
+TARGET_DEBIAN_DIR="${PROJECT_DIR}/target"
+CURRENT_POM_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 FINAL_VERSION=${CURRENT_POM_VERSION}
 NEW_VERSION=""
 
@@ -19,12 +21,12 @@ fi
 
 if [[ "$1" == "close-version" ]]; then    
     if [[ ! -z "$(git status --porcelain)" ]]; then 
-        echo 'You need to clean your changes before continue'
+        echo 'Você não pode ter arquivos alterados no git'
         exit 1
     fi
     FINAL_VERSION=$(echo "${CURRENT_POM_VERSION}" | sed 's/-SNAPSHOT//')
-    echo "Current develop version: ${CURRENT_POM_VERSION} - Release version is going to be: ${FINAL_VERSION}"
-    echo "Set the new develop version (it is not necessary to write '-SNAPSHOT', only the version number):"
+    echo "Versão atual de desenvolvimento: ${CURRENT_POM_VERSION} - Versão de produção será: ${FINAL_VERSION}"
+    echo "Defina a nova versão (não é necessário escrever '-SNAPSHOT', somente o número da versão):"
     read NEW_VERSION
     mvn versions:set -DnewVersion=${FINAL_VERSION}
     mvn clean install -DskipTests
@@ -33,7 +35,7 @@ if [[ "$1" == "close-version" ]]; then
         exit 1
     fi    
     git add pom.xml
-    git commit -m "Version ${FINAL_VERSION} finished"
+    git commit -m "Versão ${FINAL_VERSION} finalizada"
     if [[ $? != 0 ]]; then
         git reset HEAD
         git checkout pom.xml
@@ -42,8 +44,8 @@ if [[ "$1" == "close-version" ]]; then
     git tag ${FINAL_VERSION}
     mvn versions:set -DnewVersion=${NEW_VERSION}-SNAPSHOT 
     git add pom.xml
-    git commit -m "New develop version ${NEW_VERSION} started"
-    echo "Check the git changes using 'git log' command and push all using 'git push --tags'"
+    git commit -m "Nova versão de desenvolvimento ${NEW_VERSION} iniciada"
+    echo "Verifique as mudanças no git usando 'git log' e empurre usando 'git push --tags'"
 else
     mvn clean install -DskipTests
 fi
@@ -51,18 +53,18 @@ fi
 echo "Pasta temporária onde será gerado o pacote: ${TEMP_DIR}"
 
 mkdir -pv "${BASE_DIR}/DEBIAN"
-sed -r "s/#VERSION#/${FINAL_VERSION}/g" /stockmarket-manager/extras/debian/control > "${BASE_DIR}/DEBIAN/control"
-cp -v /stockmarket-manager/extras/debian/preinst "${BASE_DIR}/DEBIAN/"
+sed -r "s/#VERSION#/${FINAL_VERSION}/g" ${PROJECT_DIR}/extras/debian/control > "${BASE_DIR}/DEBIAN/control"
+cp -v ${PROJECT_DIR}/extras/debian/preinst "${BASE_DIR}/DEBIAN/"
 chmod +x "${BASE_DIR}/DEBIAN/preinst"
-cp -v /stockmarket-manager/extras/debian/postinst "${BASE_DIR}/DEBIAN/"
+cp -v ${PROJECT_DIR}/extras/debian/postinst "${BASE_DIR}/DEBIAN/"
 chmod +x "${BASE_DIR}/DEBIAN/postinst"
 
 mkdir -pv "${BASE_DIR}/lib/systemd/system"
-cp -v /stockmarket-manager/extras/debian/stockmarket-manager.service "${BASE_DIR}/lib/systemd/system/"
+cp -v ${PROJECT_DIR}/extras/debian/stockmarket-manager.service "${BASE_DIR}/lib/systemd/system/"
 
 mkdir -pv  "${BASE_DIR}/opt/comprando-valor/stockmarket-manager"
 
-cp -v /stockmarket-manager/target/stockmarket-*.jar "${BASE_DIR}/opt/comprando-valor/stockmarket-manager/stockmarket.jar"
+cp -v ${PROJECT_DIR}/target/stockmarket-*.jar "${BASE_DIR}/opt/comprando-valor/stockmarket-manager/stockmarket.jar"
 
 dpkg-deb --build "${BASE_DIR}" "${TARGET_DEBIAN_DIR}"
 
