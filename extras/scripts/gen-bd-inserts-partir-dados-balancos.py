c84c732c1e1449973feb00d3470f90bc85e4bf4b
Murilo Arantes <murilo.arantess@gmail.com>
2021-02-16 00:20:05 -0300

    correção do json; geração dos scripts de insert dos balanços

--- extras/scripts/gen-bd-inserts-partir-dados-balancos.py
+++ extras/scripts/gen-bd-inserts-partir-dados-balancos.py
@@ -115,7 +115,7 @@ def gen_balancos(empresas):
                     values = [b['trimestre'], b['ano'], patrimonio_liquido, lucro_liquido, divida_bruta, disponibilidades, ebit, liquidez_corrente, receita_liquida, e['cnpj']]
                     query = '''insert into balanco
                         (trimestre, ano, patrimonioliquido, lucroliq_trimestral, dividabruta, caixadisponivel, ebit, liquidez_corrente, receita_liquida, id_empresa, isdailyupdated)
-                        values ('{}', {}, {}, {}, {}, {}, {}, {}, {}, (select id from Empresa where cnpj = '{}'), false);\n'''.format(*values)
+                        values ({}, {}, {}, {}, {}, {}, {}, {}, {}, (select id from Empresa where cnpj = '{}'), false);\n'''.format(*values)
                     inserts.append(query)
                 except Exception as ex:
                     logging.error('Erro no processamento da empresa {} do balanco {}'.format(e['razao_social'], b))
